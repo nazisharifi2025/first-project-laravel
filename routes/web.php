3544308a3ddb152348be7f71baa0e63ce246e53b
nazi sharifi <banonazisharifi@gmail.com>
2025-11-03 20:51:09 +0430

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// ROUTE FOR TESTING PURPOSES
+ROUTE::get('/test', function(){
+    return "This is a test route.";
+});
+// route and with view
+route::get("about", function(){
+    return view("about");
+});
+// GROUPING ROUTING
+
+Route::prefix("/admin")->group(function(){
+    Route::get("/Dashbord" , function(){
+    return "This is Admin details page";
+});
+route::get("/user" , function(){
+    return "this is admin user page";
+});
+});
+// DAYNAMIK ROUTING
+Route::get('/student/{id}', function ($id) {
+    return "the Student id is " . $id;
+});
+// Route::get('/user/{id}', function ($id) {
+//     return "شناسه کاربر: " . $id;
+// });
